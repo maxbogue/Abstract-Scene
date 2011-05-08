8d8160466203f08dcecd226415a4e3bb76369c7e
Max Bogue <elnaril@gmail.com>
2011-05-08 19:04:04 -0400

    Pretty things

--- project3.cpp
+++ project3.cpp
@@ -4,39 +4,77 @@
 #   include <GL/glut.h>
 #endif
 
+#include <stdlib.h>
 #include <iostream>
 #include <cmath>
 
+#define PI 3.14159265
+
 using namespace std;
+
+bool solid;
 
 int width = 500;
 int height = 500;
 
 // Eye coords.
-int ex = 0, ey = 0, ez = 0;
-GLdouble dx = 1, dy = 0, dz = 0;
+int ex, ey, ez;
+// Delta from the eye to the reference.
+GLdouble dx, dy, dz;
 
-int rotOffset[3];
+double rotOffset[3];
+
+void reset() {
+    solid = false;
+    ex = -20;
+    ey = 0;
+    ez = 0;
+    dx = 1;
+    dy = 0;
+    dz = 0;
+    rotOffset[0] = 0;
+    rotOffset[1] = 0;
+    rotOffset[2] = 0;
+}
 
 void init(void) {
+    reset();
     glClearColor(0.0, 0.0, 0.0, 0.0); 
     glShadeModel(GL_SMOOTH);
     glEnable(GL_DEPTH_TEST);
 }
 
-void makeSphereRing(float y, float r, int sphereR, int n) {
+void makeSphereRing(float y, float r, int sphereR) {
     glPushMatrix();
+    int n = (int) (PI * r / sphereR / 2);
     float angle = 360.0 / n;
-    // glTranslatef(r,0,0);
-    glRotatef(rotOffset[0], 0.0, 1.0, 0.0);
+    glTranslatef(0,y,0);
     for (int i = 0; i < n; i++) {
         glRotatef(angle, 0.0, 1.0, 0.0);
         glPushMatrix();
         glTranslatef(r,0,0);
-        glutSolidSphere(sphereR, 100, 100);
+        if (solid) {
+            glutSolidSphere(sphereR, 15, 15);
+        } else {
+            glutWireSphere(sphereR, 15, 15);
+        }
         glPopMatrix();
     }
     glPopMatrix();
+}
+
+void makeSphereSphere(float r, int sr) {
+    int n = (int) (r / sr / 2);
+    float yStep = 2 * r / n;
+    float y = yStep / 2 - r;
+    for (int i = 0; i < n; i++) {
+        glPushMatrix();
+        glRotatef(rotOffset[i % 2], 0.0, 1.0, 0.0);
+        float x = sqrt(r*r - y*y);
+        makeSphereRing(y, x, sr);
+        y += yStep;
+        glPopMatrix();
+    }
 }
 
 void display(void) {
@@ -45,19 +83,27 @@ void display(void) {
     glPushMatrix();
     gluLookAt(ex, ey, ez, ex + dx, ey - dy, ez + dz, 0.0, 1.0, 0.0);
     
+    // Make the enclosing sphere.
     glColor3f(1.0, 0.0, 1.0);
-    glutSolidSphere(50, 100, 1, 100);
+    if (solid) {
+        glutSolidSphere(50, 100, 100);
+    } else {
+        glutWireSphere(50, 100, 100);
+    }
+    // Make the sphere of spheres touching the outer sphere.
+    glColor3f(1.0, 1.0, 0.0);
+    makeSphereSphere(47, 5);
     // glLoadIdentity();
     // gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     // glScalef(1.0, 2.0, 1.0);      /* modeling transformation */ 
     // glRotatef(spin_y, 1.0, 0.0, 0.0);
     // glRotatef(spin_x, 0.0, 1.0, 0.0);
-    glColor3f(1.0, 1.0, 0.0);
+    glColor3f(0.0, 1.0, 0.0);
     // glTranslatef(5,0,0);
     // glutSolidSphere(1, 100, 100);
     // glTranslatef(5,0,0);
     // glutSolidSphere(1, 100, 100);
-    makeSphereRing(0, 10, 8);
+    makeSphereSphere(10, 1);
     glPopMatrix();
     
     glutSwapBuffers();
@@ -68,8 +114,7 @@ void reshape(int w, int h) {
     glViewport(0, 0, (GLsizei) w, (GLsizei) h); 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(90, width / (GLdouble) height, 1, 100);
-    // glFrustum(-100, 100, -100, 100, 1, 100);
+    gluPerspective(90, w / (GLdouble) h, 1, 120);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
@@ -90,8 +135,8 @@ void motion( int x, int y ) {
 }
 
 void timer(int v) {
-    rotOffset[0] = (rotOffset[0] + 1) % 360;
-    rotOffset[1] = (rotOffset[1] + 2) % 360;
+    rotOffset[0] = (rotOffset[0] + 0.25);
+    rotOffset[1] = (rotOffset[1] + 359.5);// % 360;
     glutTimerFunc(15, timer, 0);
     glutPostRedisplay();
 }
@@ -108,6 +153,10 @@ void keyboard(unsigned char key, int x, int y) {
         case 'a': ez--; break;
         case ' ': ey++; break;
         case 'z': ey--; break;
+        case '1': solid = false; break;
+        case '2': solid = true; break;
+        case 'r': reset(); break;
+        case 'q': exit(0);
     }
 }
 
@@ -116,14 +165,14 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     // glutGameModeString("640x480:16@60");
     // glutEnterGameMode();
-    glutInitWindowSize(500, 500); 
+    glutInitWindowSize(width, height); 
     glutInitWindowPosition(100, 100);
     glutCreateWindow(argv[0]);
     init();
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
     glutDisplayFunc(display);
-    glutTimerFunc(10, timer, 0);
+    glutTimerFunc(20, timer, 0);
     glutIdleFunc(idle);
     glutPassiveMotionFunc( motion );
     glutMouseFunc( mouse );
